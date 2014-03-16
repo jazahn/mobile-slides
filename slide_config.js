0fd701c65b84d2292be8abf15c15881d10009b9d
jazahn <jcleveng@fas.harvard.edu>
2014-03-15 22:32:58 -0400

    edited the slide_config

--- slide_config.js
+++ slide_config.js
@@ -1,15 +1,15 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Mobile Web Apps',
+    subtitle: '',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/favicon.ico',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -19,12 +19,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
+    name: 'JaZahn Clevenger',
+    company: 'Sr Software Engineer',
     gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    twitter: '@jazahn',
+    www: 'http://blogs.law.harvard.edu/acts',
+    github: 'http://github.com/jazahn'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
